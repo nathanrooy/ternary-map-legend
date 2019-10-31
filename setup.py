690cfa1eec71f8b437f967dabeef62c9f65b66d9
Nathan <nathanrooy@gmail.com>
2019-10-30 22:03:26 -0400

    making it pip installable

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='tricolor',
+    version='0.0.1',
+    url='https://github.com/nathanrooy/python-svg-color-triangle.git',
+    packages=['tricolor']
+)
---
+++ tricolor/__init__.py
@@ -0,0 +1,3 @@
+import svg_color_triangle
+
+something = svg_color_triangle
